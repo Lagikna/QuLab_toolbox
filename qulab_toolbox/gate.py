5e80b08c85925777b648a88e213ed25095d00072
hanzhikun <zkhan@foxmail.com>
2018-12-04 13:54:47 +0800

    modify gate

--- qulab_toolbox/gate.py
+++ qulab_toolbox/gate.py
@@ -2,7 +2,9 @@ import numpy as np
 from qulab.waveform import *
 
 #Created by hanzhikun
+#Email: zkhan@foxmail.com
 #Created date:2018.11.22
+
 #Data Input:
 #1.para         :Gaussian width,awg_freq,amplitude,IM_phas,IM_amp
 #2.delay        :Move the Waveform 'delay' time(unit in us)
@@ -15,10 +17,9 @@ from qulab.waveform import *
 class xy_gauss():
     ''' XY gate: Gauss;with DRAG'''
 
-    def __init__(self,para,delay,theta=0,alpha=0,range=[0,50.0],samplerate=2.0e9):
+    def __init__(self,para,delay,theta=0,alpha=0,samplerate=2.0e9):
         self.para=para
         self.delay=delay
-        self.range=range
         self.theta=theta
         self.alpha=alpha
         self.samplerate=samplerate/1.0e6
@@ -36,24 +37,36 @@ class xy_gauss():
         _drag_gauss=(Drag_gauss(self.para[0],self.samplerate)<<(self.para[0]/2))
 
         #mutiply with Sin wave(differ phase for differ gate) and gauss DRAG.(alpha modify the drag parameter)
-        _sqAI= (((_cell*Sin(2*np.pi*self.para[1],np.mod(np.deg2rad(self.theta),360.0))*self.para[2])+\
+        self._sqAI= (((_cell*Sin(2*np.pi*self.para[1],np.mod(np.deg2rad(self.theta),360.0))*self.para[2])+\
         (_drag_gauss*Sin(2*np.pi*self.para[1],np.mod(np.deg2rad(self.theta)+90.0,360.0))*self.para[2]*self.alpha))*\
         (DC(1,self.para[0])<<self.para[0]))>>self.delay
 
-        _sqAQ= (((_cell*Sin(2*np.pi*self.para[1],np.mod(np.deg2rad(self.para[3]+self.theta),360.0))*self.para[2]*self.para[4])+\
+        self._sqAQ= (((_cell*Sin(2*np.pi*self.para[1],np.mod(np.deg2rad(self.para[3]+self.theta),360.0))*self.para[2]*self.para[4])+\
         (_drag_gauss*Sin(2*np.pi*self.para[1],np.mod(np.deg2rad(self.para[3]+self.theta)+90.0,360.0))*self.para[2]*self.para[4]*self.alpha))*\
         (DC(1,self.para[0])<<self.para[0]))>>self.delay
-
-        #generate gate (detail see qulab.waveform)
-        _I=_sqAI.set_range(self.range[0],self.range[1]).generateData(sampleRate=(self.samplerate))
-        _Q=_sqAQ.set_range(self.range[0],self.range[1]).generateData(sampleRate=(self.samplerate))
-        self._sqAI=_I
-        self._sqAQ=_Q
-        return self._sqAI,self._sqAQ
 
     @property
     def wave(self):
         self._gate()
+        return self._sqAI,self._sqAQ
+
+class xy_pts(self,):
+    ''' generate IQ dual channel waveform datat points'''
+    def __init__(self,sqA,sqB,range=[0,50.0],samplerate=2.0e9):
+        self.range=range
+        self.samplerate=samplerate/1.0e6
+        self._I=sqA
+        self._Q=sqB
+        self._sqAI=None
+        self._sqAQ=None
+
+    def _pts(self):
+        self._sqAI=self._I.set_range(self.range[0],self.range[1]).generateData(sampleRate=(self.samplerate))
+        self._sqAQ=self._Q.set_range(self.range[0],self.range[1]).generateData(sampleRate=(self.samplerate))
+
+    @property
+    def wave(self):
+        self._pts()
         return self._sqAI,self._sqAQ
 
 class z_square():
