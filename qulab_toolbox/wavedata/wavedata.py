af6c1df4afc8b621ac13c3ebaec60ded294adff6
Li Zhiyuan <32617128+Lagikna@users.noreply.github.com>
2018-12-19 15:02:44 +0800

    change sampleRate to sRate

--- qulab_toolbox/wavedata/wavedata.py
+++ qulab_toolbox/wavedata/wavedata.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 
 class Wavedata(object):
 
-    def __init__(self,domain=(0,1),sampleRate=1e2):
+    def __init__(self,domain=(0,1),sRate=1e2):
         '''domain: 定义域，即采点的区域，不能是inf'''
         self._domain = domain
         self.len = domain[1] - domain[0]
-        self.sampleRate = sampleRate
+        self.sRate = sRate
         # self._timeFunc = lambda x : 0
         self.data = self.generateData()
 
@@ -24,14 +24,14 @@ class Wavedata(object):
         return self._mask(x)*self._timeFunc(x)
 
     def generateData(self):
-        dt=1/self.sampleRate
+        dt=1/self.sRate
         x = np.arange(self._domain[0]+dt/2, self._domain[1], dt)
         _data = self.__timeFunc(x)
         data = np.array(_data)
         return data
 
     def _blank(self,length=0):
-        n = int(abs(length)*self.sampleRate)
+        n = int(abs(length)*self.sRate)
         data = np.zeros(n)
         return data
 
@@ -39,11 +39,11 @@ class Wavedata(object):
         return self.len
 
     def size(self):
-        size = int(self.len*self.sampleRate)
+        size = int(self.len*self.sRate)
         return size
 
     def setLen(self,length):
-        n = int(abs(length)*self.sampleRate)
+        n = int(abs(length)*self.sRate)
         l = self.size()
         if n >l:
             append_data=np.zeros(n-l)
@@ -58,14 +58,14 @@ class Wavedata(object):
     #
     def __neg__(self):
         w = Wavedata()
-        w.sampleRate = self.sampleRate
+        w.sRate = self.sRate
         w.len = self.len
         w.data = -self.data
         return w
 
     def __abs__(self):
         w = Wavedata()
-        w.sampleRate = self.sampleRate
+        w.sRate = self.sRate
         w.len = self.len
         w.data = np.abs(self.data)
         return w
@@ -74,10 +74,10 @@ class Wavedata(object):
         if abs(t)>self.len:
             raise Error('shift is too large !')
         w = Wavedata()
-        w.sampleRate = self.sampleRate
+        w.sRate = self.sRate
         w.len = self.len
         shift_data=self._blank(abs(t))
-        left_n = int((self.len-abs(t))*self.sampleRate)
+        left_n = int((self.len-abs(t))*self.sRate)
         if t>0:
             w.data = np.append(shift_data, self.data[:left_n])
         else:
@@ -90,22 +90,22 @@ class Wavedata(object):
     def __or__(self, other):
         if not isinstance(other,Wavedata):
             raise TypeError('not Wavedata class !')
-        elif not self.sampleRate == other.sampleRate:
-            raise Error('sampleRate not equal !')
+        elif not self.sRate == other.sRate:
+            raise Error('sRate not equal !')
         else:
             w = Wavedata()
-            w.sampleRate = self.sampleRate
+            w.sRate = self.sRate
             w.len = self.len + other.len
             w.data = np.append(self.data,other.data)
             return w
 
     def __add__(self, other):
         if isinstance(other,Wavedata):
-            if not self.sampleRate == other.sampleRate:
-                raise Error('sampleRate not equal !')
+            if not self.sRate == other.sRate:
+                raise Error('sRate not equal !')
             else:
                 w = Wavedata()
-                w.sampleRate = self.sampleRate
+                w.sRate = self.sRate
                 length = max(self.len, other.len)
                 self.setLen(length)
                 other.setLen(length)
@@ -117,7 +117,7 @@ class Wavedata(object):
 
     def __radd__(self, v):
         w = Wavedata()
-        w.sampleRate = self.sampleRate
+        w.sRate = self.sRate
         w.len = self.len
         w.data = self.data +v
         return w
@@ -130,11 +130,11 @@ class Wavedata(object):
 
     def __mul__(self, other):
         if isinstance(other,Wavedata):
-            if not self.sampleRate == other.sampleRate:
-                raise Error('sampleRate not equal !')
+            if not self.sRate == other.sRate:
+                raise Error('sRate not equal !')
             else:
                 w = Wavedata()
-                w.sampleRate = self.sampleRate
+                w.sRate = self.sRate
                 length = max(self.len, other.len)
                 self.setLen(length)
                 other.setLen(length)
@@ -146,18 +146,18 @@ class Wavedata(object):
 
     def __rmul__(self, v):
         w = Wavedata()
-        w.sampleRate = self.sampleRate
+        w.sRate = self.sRate
         w.len = self.len
         w.data = self.data * v
         return w
 
     def __truediv__(self, other):
         if isinstance(other,Wavedata):
-            if not self.sampleRate == other.sampleRate:
-                raise Error('sampleRate not equal !')
+            if not self.sRate == other.sRate:
+                raise Error('sRate not equal !')
             else:
                 w = Wavedata()
-                w.sampleRate = self.sampleRate
+                w.sRate = self.sRate
                 length = max(self.len, other.len)
                 self.setLen(length)
                 other.setLen(length)
@@ -169,13 +169,13 @@ class Wavedata(object):
 
     def __rtruediv__(self, v):
         w = Wavedata()
-        w.sampleRate = self.sampleRate
+        w.sRate = self.sRate
         w.len = self.len
         w.data = v / self.data
         return w
 
     def plot(self):
-        dt=1/self.sampleRate
+        dt=1/self.sRate
         x = np.arange(dt/2, self.len, dt)
         y = self.data
         plt.plot(x, y)
@@ -183,41 +183,41 @@ class Wavedata(object):
 
 class Blank(Wavedata):
     '''产生一个给定长度的0波形，长度可以为负或0'''
-    def __init__(self, width=0, sampleRate=1e2):
+    def __init__(self, width=0, sRate=1e2):
         self.start = min(0,width)
         self.stop = max(0,width)
-        super(Blank, self).__init__(domain=(self.start, self.stop),sampleRate=sampleRate)
+        super(Blank, self).__init__(domain=(self.start, self.stop),sRate=sRate)
 
 class DC(Wavedata):
-    def __init__(self, offset, width=0, sampleRate=1e2):
+    def __init__(self, offset=1, width=0, sRate=1e2):
         self.start = min(0,width)
         self.stop = max(0,width)
         self._timeFunc = lambda x : offset
-        super(Blank, self).__init__(domain=(self.start, self.stop),sampleRate=sampleRate)
+        super(Blank, self).__init__(domain=(self.start, self.stop),sRate=sRate)
 
 class Gaussian(Wavedata):
-    def __init__(self, width, sampleRate=1e2):
+    def __init__(self, width, sRate=1e2):
         c = width/(4*np.sqrt(2*np.log(2)))
         self._timeFunc = lambda x: np.exp(-0.5*(x/c)**2)
-        super(Gaussian, self).__init__(domain=(-0.5*width,0.5*width),sampleRate=sampleRate)
+        super(Gaussian, self).__init__(domain=(-0.5*width,0.5*width),sRate=sRate)
 
 class Sin(Wavedata):
-    def __init__(self, w, phi=0, width=0, sampleRate=1e2):
+    def __init__(self, w, phi=0, width=0, sRate=1e2):
         self._timeFunc = lambda t: np.sin(w*t+phi)
-        super(Sin, self).__init__(domain=(0,width),sampleRate=sampleRate)
+        super(Sin, self).__init__(domain=(0,width),sRate=sRate)
 
 class Cos(Wavedata):
-    def __init__(self, w, phi=0, width=0, sampleRate=1e2):
+    def __init__(self, w, phi=0, width=0, sRate=1e2):
         self._timeFunc = lambda t: np.cos(w*t+phi)
-        super(Cos, self).__init__(domain=(0,width),sampleRate=sampleRate)
+        super(Cos, self).__init__(domain=(0,width),sRate=sRate)
 
 __all__ = ['Wavedata', 'Blank', 'DC', 'Gaussian', 'Sin', 'Cos',]
 
 
 if __name__ == "__main__":
-    a=Sin(w=1, width=10, phi=0, sampleRate=100000)
-    b=Gaussian(2,sampleRate=100000)
-    c=Wavedata(sampleRate=100000)
+    a=Sin(w=1, width=10, phi=0, sRate=100000)
+    b=Gaussian(2,sRate=100000)
+    c=Wavedata(sRate=100000)
 
     m=0.5*a|c|b|c|b+1|c|a+0.5
     m.plot()
