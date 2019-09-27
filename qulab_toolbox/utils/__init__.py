0c5ee8c1bc8ce35f4375135049784a26ea9b5868
Li Zhiyuan <32617128+Lagikna@users.noreply.github.com>
2019-09-27 09:32:20 +0800

    fix sth

--- qulab_toolbox/utils/__init__.py
+++ qulab_toolbox/utils/__init__.py
@@ -4,8 +4,9 @@ class d2c(object):
     '''dict to class
     
     将字典的调用方式转化为类属性的调用方式；
-    字典的关键字须符合类属性的规范，可以赋值'''
-
+    字典的关键字须符合类属性命名的规范，可以赋值
+    '''
+    
     def __init__(self,d):
         '''传入一个字典或者d2c类的实例，构造一个d2c实例'''
         if isinstance(d,d2c):
@@ -16,7 +17,7 @@ class d2c(object):
             self.__dict__.update({k:_v})
             
     def todict(self):
-        '''从d2c实例提取出字典'''
+        '''从d2c实例提取出原字典'''
         d={}
         for k,v in self.__dict__.items():
             _v=v.todict() if isinstance(v,d2c) else v
